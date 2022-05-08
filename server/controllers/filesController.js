01d79b7f097dae866ee6ea5e654a91d1d6ca7c93
rokuwu <rookuczi@gmail.com>
2022-05-08 12:11:10 +0200

    file uploading

--- server/controllers/filesController.js
+++ server/controllers/filesController.js
@@ -2,37 +2,54 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const File = require('../models/fileModel');
+const asyncHandler = require('express-async-handler');
+const multer = require('multer');
+const path = require('path');
 
-const uploadFile = (req, res) => {
+const uploadFile = asyncHandler(async (req, res) => {    
+    console.log(req.file);
+
     res.status(200).json({
         function: 'uploadFile'
     });
-}
+});
 
-const deleteFile = (req, res) => {
+const deleteFile = asyncHandler(async (req, res) => {
     res.status(200).json({
         function: 'deleteFile'
     });
-}
+});
 
-const updateFile = (req, res) => {
+const updateFile = asyncHandler(async (req, res) => {
     res.status(200).json({
         function: 'updateFile'
     });
-}
+});
 
-const deleteAllFiles = (req, res) => {
+const deleteAllFiles = asyncHandler(async (req, res) => {
     res.status(200).json({
         function: 'deleteAllFiles'
     });
-}
+});
 
-const getFiles = (req, res) => {
+const getFiles = asyncHandler(async (req, res) => {
     res.status(200).json({
         function: 'getFiles'
     });
-}
+});
+
+// multer storage
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {    
+        const uploadPath = path.join(__dirname + '/../uploads');    
+        cb(null, uploadPath);
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`);
+    }
+});
+const upload = multer({ storage: storage });
 
 module.exports = {
-    uploadFile, deleteFile, updateFile, deleteAllFiles, getFiles
+    uploadFile, deleteFile, updateFile, deleteAllFiles, getFiles, upload
 }
--- server/routes/filesRouter.js
+++ server/routes/filesRouter.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const filesRouter = express.Router();
-const { uploadFile, deleteFile, updateFile, deleteAllFiles, getFiles } = require('../controllers/filesController');
+const { uploadFile, deleteFile, updateFile, deleteAllFiles, getFiles, upload } = require('../controllers/filesController');
 const { protect } = require('../middleware/protectRoutes');
 
-filesRouter.post('/upload', protect, uploadFile);
+filesRouter.post('/upload', protect, upload.single('file'), uploadFile);
 filesRouter.put('/update/:id', protect, updateFile);
 filesRouter.delete('/delete/:id', protect, deleteFile);
 filesRouter.delete('/delete', protect, deleteAllFiles);
